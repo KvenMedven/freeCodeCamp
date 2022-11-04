5d912820fe06fdbd111dd6aa894c70eff9c56f18
Artur_Islakaev <artur_islakaev@mail.ru>
2022-11-04 20:31:28 +0500

    Basic JavaScript_103

--- JS/Basic JavaScript/103.Vowel one.js
+++ JS/Basic JavaScript/103.Vowel one.js
@@ -16,63 +16,10 @@ function vowelOne(s){
                 str +='1'
                 break;
 
-            case "b":
-            case "B":
-            case "c":
-            case "C":
-            case "d":
-            case "D":
-            case "f":
-            case "F":
-            case "g":
-            case "G":
-            case "h":
-            case "H":
-            case "j":
-            case "J":
-            case "k":
-            case "K":
-            case "l":
-            case "L":
-            case "m":
-            case "M":
-            case "n":
-            case "N":
-            case "p":
-            case "P":
-            case "q":
-            case "Q":
-            case "r":
-            case "R":
-            case "s":
-            case "S":
-            case "t":
-            case "T":
-            case "v":
-            case "V":
-            case "w":
-            case "W":
-            case "x":
-            case "X":
-            case "z":
-            case "Z":
-            case " ":
-            case ",":
-            case ".":
-            case ":":
-            case "0":
-            case "1":
-            case "2":
-            case "3":
-            case "4":
-            case "5":
-            case "6":
-            case "7":
-            case "8":
-            case "9":
-
+            default :
                 str +='0'
                 break;
+
 
         }
     } return str;
